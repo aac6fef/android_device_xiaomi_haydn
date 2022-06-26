f97923f2a8ec27806e0dc185db7059f4ca78c625
gaozexuan <gaozexuan6436@outlook.com>
2022-06-26 12:58:07 +0800

    modified:   BoardConfig.mk modified:   device.mk deleted:    prebuild/image.gz modified:   twrp_haydn.mk

--- device.mk
+++ device.mk
@@ -2,13 +2,6 @@
 # Copyright (C) 2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
-#
-
-# Inherit from sm8350-common
-$(call inherit-product, device/xiaomi/sm8350-common/common.mk)
-
-# Inherit proprietary targets
-$(call inherit-product-if-exists, vendor/xiaomi/haydn/haydn-vendor.mk)
 
 
 # Soong namespaces
